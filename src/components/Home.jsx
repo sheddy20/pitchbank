5e188e98c1a4d6dd6af9a34520e18dd0c4651224
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-01 17:21:22 +0100

    'changes'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 
 function Home () {
     const info = {
-        name: 'Alesso',
-        location: 'fortcollins Wincosin',
-        age: 30,
-        sex: 'male',
-        isMarried: 'Single',
+        // name: 'Alesso',
+        // location: 'fortcollins Wincosin',
+        // age: 30,
+        // sex: 'male',
+        // isMarried: 'Single',
+        subtitle: 'welcome to javascript mastery',
+        users: ['one', 'two', 'three', 'four', 'five',  'seven'],
     }
-
-    
 
     return(
         <div>
+             {info.users.length >= 7 && <p>{info.users}</p>}
+            <p className="App">{info.subtitle ? info.subtitle : 'subtitle not found'}</p>
             <h1>Name: {info.name ? info.name : 'name props not found'}</h1>
             <h1>Location: {info.location ? info.location : 'No name'}</h1>
             <h1>Gender: {info.sex ? info.sex : 'Not provided'}</h1>
