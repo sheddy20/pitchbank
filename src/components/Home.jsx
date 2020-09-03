d9c7bef27e6f3ab487df1182ed6d9a875668ec21
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-03 23:32:58 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Home () {
-    function getName () {
-        let name = 'Hal jordan';
-        return name;
-    }
-    const names = getName('Jordan Peterson'); 
+export default function () {
+    const [count, setCount] = useState(0);
     return (
-        <div>
-           <h1>{names}</h1>
+        <div className="div-app">
+            <h1>State With Functional Componoents In React.</h1>
+            <p className='p-app'>You clicked {count} times.</p>
+            <button onClick={() => setCount(count + 1)} className="b-app">
+                Tap Me.
+            </button>
         </div>
     );
 }
