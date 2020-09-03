4edacb0603e4e22179eb96323f69951e3c46bd18
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-03 23:40:20 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,8 @@ export default function () {
     const [count, setCount] = useState(0);
     return (
         <div className="div-app">
-            <h1>State With Functional Componoents In React.</h1>
+            <h1>Functional Componoents With State.</h1>
+            <h3 className="h-app">React Hooks</h3>
             <p className='p-app'>You clicked {count} times.</p>
             <button onClick={() => setCount(count + 1)} className="b-app">
                 Tap Me.
