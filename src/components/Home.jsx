dd08474cafc4c5d42dc35413b6e6d278abc71bf2
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-03 22:45:36 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 
-
-function Home () {
+export default function Home () {
     const info = {
-        // name: 'Alesso',
-        // location: 'fortcollins Wincosin',
-        // age: 30,
-        // sex: 'male',
-        // isMarried: 'Single',
-        subtitle: 'welcome to javascript mastery',
-        users: ['one', 'two', 'three', 'four', 'five',  'seven'],
+        name: 'Drew Barrymore',
+        title: 'Miss universe',
+        awards: ['Grammy', 'Emmy awards', 'Oscar', 'E choice'],
     }
-
-    return(
+    return (
         <div>
-             {info.users.length >= 7 && <p>{info.users}</p>}
-            <p className="App">{info.subtitle ? info.subtitle : 'subtitle not found'}</p>
-            <h1>Name: {info.name ? info.name : 'name props not found'}</h1>
-            <h1>Location: {info.location ? info.location : 'No name'}</h1>
-            <h1>Gender: {info.sex ? info.sex : 'Not provided'}</h1>
-            <h1>Marrital Status: {info.isMarried ? info.isMarried : 'Unknown'}</h1>
-            {info.age >= 18 && <p className="one">Age:{info.age}</p> ? <p classNam="two">{info.age}</p> : <p>Age not found</p>}
+            {info.name && <p>{info.name}</p>}
+            {info.title && <p>{info.title}</p>}
+            <p>{info.awards.length < 0 ? 'Awards is correct' : 'WRONG!!'}</p>
         </div>
     );
 }
-
-export default Home;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
-import axios from "axios";
-
+import Axios from "axios";
 
 
 class Title extends Component {
     state = {
         users: [],
     }
-    componentDidMount () {
-         axios.get(`https://jsonplaceholder.typicode.com/users`)
-              .then(res => {
-                  const users = res.data;
-                  this.setState({ users })
-              });
+    componentDidMount() {
+        Axios.get(`https://jsonplaceholder.typicode.com/users`)
+             .then(res =>{
+                 const users = res.data;
+                 this.setState({ users });
+             });
     }
     render() {
-        const info = {
-            name: 'shedrack',
-            location: 'San Francisco',
-            age: 40,
-            country: 'United State Of America'
-        }
         return (
             <div>
-                <h1>users info in listview</h1>
-                <ul>
+                <ol>
                     {this.state.users.map(m=> <li>{m.name}</li>)}
-                </ul>
-                <h2>Name: {info.name}</h2>
+                </ol>
             </div>
         );
     }
