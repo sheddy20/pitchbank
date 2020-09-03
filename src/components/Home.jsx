674be464596145c7fad4e2d59746a16546f65939
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-03 23:04:19 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 export default function Home () {
-    const info = {
-        name: 'Drew Barrymore',
-        title: 'Miss universe',
-        awards: ['Grammy', 'Emmy awards', 'Oscar', 'E choice'],
+    function getName () {
+        let name = 'Hal jordan';
+        return name;
     }
+    const names = getName('Jordan Peterson'); 
     return (
         <div>
-            {info.name && <p>{info.name}</p>}
-            {info.title && <p>{info.title}</p>}
-            <p>{info.awards.length < 0 ? 'Awards is correct' : 'WRONG!!'}</p>
+           <h1>{names}</h1>
         </div>
     );
 }
