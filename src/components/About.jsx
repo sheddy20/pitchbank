cfa8f8b1d10ea0f901b94af4177d291122457e9e
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-06 10:59:33 +0100

    'changes'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,15 @@ export default function About () {
     const myAge = (num1, num2) => {
         return num1 - num2;
     }
+    const negative = (y) => {
+        return y + y;
+    }
+    const negations = negative(20);
     const checkAge = myAge(2020, 1998);
     const result = positive(10);
     return (
         <div>
+            <h1>{negations}</h1>
             <h1>{result}</h1>
             <h1>{`you are: ${checkAge} years old...`}</h1>
         </div>
