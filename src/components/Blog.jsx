904ed42143c669fea7c6bf6b49aecf3a7b4eef91
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-06 14:17:36 +0100

    'changes'

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Blog () {
-    const users = {
-        name: 'Shedrack',
-        placesVisited: ['Upsala', 'Ontario', 'lagos', 'Amsterdam'],
-        placesLived: function () {
-            return <div>
-                <h1>{`${this.name}`}</h1>
-                <h1>{`${this.placesVisited}`}</h1>
-            </div>
-        }
-    }
-    const details = users.placesLived();
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        document.title = `you click the button ${counter} times`
+    });
+
     return (
         <div>
-            <h1>{details}</h1>
-            <h1>Hello world</h1>
+            <p>you have tap the button {counter} times</p>
+            <button onClick={()=> setCounter(counter + 1)}>
+                Press the Button..
+            </button>
         </div>
     );
 }
